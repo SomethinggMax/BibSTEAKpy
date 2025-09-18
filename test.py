23323fa2120ff7fc681e4f4d4f60f28de9f01fbf
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-18 13:43:48 +0200

    Basic implementation for bib file parsing. Dict of {(ref_type, key), data}.

---
+++ test.py
@@ -0,0 +1,42 @@
+from enum import Enum
+import pprint
+
+
+class Token(Enum):
+    REF_TYPE = 1
+    KEY = 2
+    DATA = 3
+
+
+references = {}
+
+with open("biblatex-examples.bib", "r+") as file:
+    token = ""
+    ref_type = ""
+    key = ""
+    token_type = Token.REF_TYPE
+    for line in file:
+        for char in line:
+            if char == "@":
+                if token_type == Token.DATA:
+                    token = token.strip()[:-1]
+                    references[(ref_type, key)] = token
+                    token = ""
+                    token_type = Token.REF_TYPE
+                continue
+            if char == "{":
+                if token_type == Token.REF_TYPE:
+                    ref_type = token
+                    token = ""
+                    token_type = Token.KEY
+                    continue
+            if char == "," or char == "=":
+                if token_type == Token.KEY:
+                    key = token.strip()
+                    token = ""
+                    token_type = Token.DATA
+                    continue
+            if char != "\n":
+                token += char
+
+pprint.pprint(references)

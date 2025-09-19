68b51d321eef33f90e9477380a8c62b5a58815f3
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-19 14:55:33 +0200

    Added extra checks to ignore text in between references.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ class Token(Enum):
     REF_TYPE = 1
     KEY = 2
     DATA = 3
+    EXTRA = 4
 
 
 def parse_string(data):
@@ -57,10 +58,15 @@ def parse_bib(file_name):
         ref_type = ""
         key = ""
         curly_bracket_level = 0
-        token_type = Token.REF_TYPE
+        token_type = Token.EXTRA
         for line in file:
             for char in line:
                 match char:
+                    case "@":
+                        if token_type == Token.EXTRA:
+                            token = ""
+                            token_type = Token.REF_TYPE
+                            continue
                     case "{":
                         curly_bracket_level += 1
                         if token_type == Token.REF_TYPE:
@@ -70,20 +76,21 @@ def parse_bib(file_name):
                             continue
                     case "}":
                         curly_bracket_level -= 1
-                        if curly_bracket_level == 0:
-                            if ref_type == "preamble" or ref_type == "comment":
-                                print("Skipping ", ref_type, "entry")
+                        if token_type == Token.DATA:
+                            if curly_bracket_level == 0:
+                                if ref_type == "preamble" or ref_type == "comment":
+                                    print("Skipping ", ref_type, "entry")
+                                    token = ""
+                                    token_type = Token.EXTRA
+                                    continue
+                                token = token.strip()
+                                if ref_type == "string":
+                                    references[(ref_type, key)] = parse_string(token)
+                                else:
+                                    references[(ref_type, key)] = parse_tags(token)
                                 token = ""
-                                token_type = Token.REF_TYPE
+                                token_type = Token.EXTRA
                                 continue
-                            token = token.strip()
-                            if ref_type == "string":
-                                references[(ref_type, key)] = parse_string(token)
-                            else:
-                                references[(ref_type, key)] = parse_tags(token)
-                            token = ""
-                            token_type = Token.REF_TYPE
-                            continue
                     case "," | "=":
                         if token_type == Token.KEY:
                             key = token.strip()

3cac1b134cf8287fd795f6aac80e8160565774d9
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-19 11:27:13 +0200

    Added tag parsing and seperated parsing in multiple functions.

--- test.py
+++ test.py
@@ -8,43 +8,82 @@ class Token(Enum):
     DATA = 3
 
 
-references = {}
+def parse_string(data):
+    return data[1:-1]
 
-with open("biblatex-examples.bib", "r+") as file:
+
+def parse_tags(data):
+    dictionary = {}
+    field_type = ""
     token = ""
-    ref_type = ""
-    key = ""
-    token_type = Token.REF_TYPE
-    for line in file:
+    curly_bracket_level = 0
+    for line in data:
         for char in line:
-            if char == "@":
-                if token_type == Token.DATA:
-                    ref_type = ref_type.lower()
-                    if ref_type == "preamble" or ref_type == "comment":
-                        print("Skipping ", ref_type, "entry")
-                        token = ""
-                        token_type = Token.REF_TYPE
-                        continue
-                    token = token.strip()[:-1]
-                    references[(ref_type, key)] = token
-                    token = ""
-                    token_type = Token.REF_TYPE
+            if char == "=" and curly_bracket_level == 0:
+                field_type = token.strip()
+                token = ""
+                continue
+            if char == "," and curly_bracket_level == 0:
+                dictionary[field_type] = token.strip()
+                token = ""
                 continue
             if char == "{":
-                if token_type == Token.REF_TYPE:
-                    ref_type = token
-                    token = ""
-                    token_type = Token.KEY
+                curly_bracket_level += 1
+                if curly_bracket_level == 1:
                     continue
-            if char == "," or char == "=":
-                if token_type == Token.KEY:
-                    key = token.strip()
-                    token = ""
-                    token_type = Token.DATA
+            if char == "}":
+                curly_bracket_level -= 1
+                if curly_bracket_level == 0:
                     continue
-            if char == "#":
-                print("String concatenation not yet supported!")
-            if char != "\n":
-                token += char
+            token += char
+    return dictionary
 
-pprint.pprint(references)
+
+def parse_bib(file_name):
+    references = {}
+
+    with open(file_name, "r+") as file:
+        token = ""
+        ref_type = ""
+        key = ""
+        token_type = Token.REF_TYPE
+        for line in file:
+            for char in line:
+                if char == "@":
+                    if token_type == Token.DATA:
+                        ref_type = ref_type.lower()
+                        if ref_type == "preamble" or ref_type == "comment":
+                            print("Skipping ", ref_type, "entry")
+                            token = ""
+                            token_type = Token.REF_TYPE
+                            continue
+                        token = token.strip()[:-1]
+                        if ref_type == "string":
+                            references[(ref_type, key)] = parse_string(token)
+                        else:
+                            references[(ref_type, key)] = parse_tags(token)
+                        token = ""
+                        token_type = Token.REF_TYPE
+                    continue
+                if char == "{":
+                    if token_type == Token.REF_TYPE:
+                        ref_type = token
+                        token = ""
+                        token_type = Token.KEY
+                        continue
+                if char == "," or char == "=":
+                    if token_type == Token.KEY:
+                        key = token.strip()
+                        token = ""
+                        token_type = Token.DATA
+                        continue
+                if char == "#":
+                    print("String concatenation not yet supported!")
+                if char != "\n":
+                    token += char
+    return references
+
+
+result = parse_bib("biblatex-examples.bib")
+
+pprint.pprint(result)

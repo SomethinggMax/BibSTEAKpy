a709fa3d07f5e92f148a1f50b1e9069b42eb3633
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-21 15:01:45 +0200

    Added bib file generator and tests to compare the generated file with the original. The file is generated from the dictionary made by the parser.

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import pprint
+import difflib
+import file_generator
 import file_parser
 
+bib_examples_original = "biblatex-examples.bib"
+bib_examples_generated = "biblatex-examples-generated.bib"
+bib_tests = "bibtests.bib"
 
-result = file_parser.parse_bib("biblatex-examples.bib", True)
-test = file_parser.parse_bib("bibtests.bib", True)
+
+result = file_parser.parse_bib(bib_examples_original, False)
+test = file_parser.parse_bib(bib_tests, True)
 
 pprint.pprint(result)
 print(test)
@@ -12,3 +18,16 @@ print(test)
 # print(result[("book", "augustine")]["author"])
 # print(result[("book", "cicero")]["annotation"])
 
+# Generate file from the dictionary:
+file_generator.generate_bib(bib_examples_generated, result, 15)
+
+# Print the differences between the original file and the generated file:
+with open(bib_examples_original) as original:
+    original_string = original.readlines()
+with open(bib_examples_generated) as generated:
+    generated_string = generated.readlines()
+for line in difflib.unified_diff(
+        original_string, generated_string,
+        fromfile=bib_examples_original, tofile=bib_examples_generated,
+        lineterm=''):
+    print(line)

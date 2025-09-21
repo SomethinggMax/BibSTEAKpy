3257f6ed05495e0261f0f69a8b9d89de4d1b152d
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-21 20:45:36 +0200

    Added test for batch_replace.

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import pprint
 import difflib
+import batch_editor
 import file_generator
 import file_parser
 
 bib_examples_original = "biblatex-examples.bib"
 bib_examples_generated = "biblatex-examples-generated.bib"
+bib_examples_edited = "biblatex-examples-edited.bib"
 bib_tests = "bibtests.bib"
 
 
-result = file_parser.parse_bib(bib_examples_original, False)
+examples = file_parser.parse_bib(bib_examples_original, False)
 test = file_parser.parse_bib(bib_tests, True)
 
-pprint.pprint(result)
+pprint.pprint(examples)
 print(test)
 
 # Some examples on how to access information from the dictionary.
@@ -19,7 +21,7 @@ print(test)
 # print(result[("book", "cicero")]["annotation"])
 
 # Generate file from the dictionary:
-file_generator.generate_bib(bib_examples_generated, result, 15)
+file_generator.generate_bib(bib_examples_generated, examples, 15)
 
 # Print the differences between the original file and the generated file:
 with open(bib_examples_original) as original:
@@ -31,3 +33,16 @@ for line in difflib.unified_diff(
         fromfile=bib_examples_original, tofile=bib_examples_generated,
         lineterm=''):
     print(line)
+
+examples_edited = batch_editor.batch_replace(
+    examples, ["publisher"],
+    "pup", "Princeton University Press")
+file_generator.generate_bib(bib_examples_edited, examples_edited, 15)
+with open(bib_examples_edited) as edited:
+    edited_string = edited.readlines()
+for line in difflib.unified_diff(
+        generated_string, edited_string,
+        fromfile=bib_examples_generated, tofile=bib_examples_edited,
+        lineterm=''):
+    print(line)
+

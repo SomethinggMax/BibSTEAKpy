6d297c43301dd7b1d3e4f1acb299fef5caf000e6
DESKTOP-UU2VB6F\tabre <tabreafabian0@gmail.com>
2025-09-22 15:59:17 +0200

    Shell update and config file

--- BibStShell.py
+++ BibStShell.py
@@ -1,14 +1,24 @@
 import cmd
 import os
 import shutil
+import json
 
 STORAGE_FOLDER = "storage"
-    
+
+CONFIG_FILE = "config.json"
+
 COMMANDS = [("help", "Display the current menu"),
             ("quit", "Close the BibSteakShell"),
-            ("load", "Load a particular file into the program's storage"),
-            ("storage", "See all the files which are curently in the storage"),
+            ("load", "Load a particular file into the working directory"),
+            ("storage", "See all the bib files in the working directory"),
+            ("wd", "Get current working directory"),
             ]
+
+def get_working_directory_path():
+    with open("config.json", "r") as f:
+        config = json.load(f)
+        working_directory_path = config["working_directory"]
+        return working_directory_path
 
 def load_file_to_storage(source_path):
     """
@@ -16,10 +26,11 @@ def load_file_to_storage(source_path):
     Creates the folder if it doesn't exist.
     """
     try:
-        os.makedirs(STORAGE_FOLDER, exist_ok=True)
+        working_directory = get_working_directory_path()
+        os.makedirs(working_directory, exist_ok=True)
         filename = os.path.basename(source_path)
         name, extension = os.path.splitext(filename)
-        destination_path = os.path.join(STORAGE_FOLDER, filename)
+        destination_path = os.path.join(working_directory, filename)
         
         if extension == ".bib":
             shutil.copy(source_path, destination_path)
@@ -31,6 +42,26 @@ def load_file_to_storage(source_path):
     except Exception as e:
         print(f"Unexpected error: {e}")
         return None
+    
+    
+def set_directory(absolute_working_directory_path):
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+            
+        config["working_directory"] = absolute_working_directory_path
+            
+        with open("config.json", "w") as f:
+            json.dump(config, f, indent=2)
+            
+        print(f"Directory successfuly set to {absolute_working_directory_path}")
+        
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return None
+        
+        
+
 
     
 def display_help_commands():
@@ -44,7 +75,7 @@ def display_help_commands():
     
 def display_storage_files():
     try:
-        folder = "storage"
+        folder = get_working_directory_path()
         for filename in os.listdir(folder):
             full_path = os.path.join(folder, filename)
             if os.path.isfile(full_path):   # ignore subfolders
@@ -57,22 +88,39 @@ def display_storage_files():
     
 
 class MyTool(cmd.Cmd):
-    intro = "Welcome to BibStShell! Type 'help' to list commands.\n"
+    intro = f"""
+    ____  _ _     _____ _______ ______          _  __   _____ _          _ _ 
+    |  _ \(_) |   / ____|__   __|  ____|   /\   | |/ /  / ____| |        | | |
+    | |_) |_| |__| (___    | |  | |__     /  \  | ' /  | (___ | |__   ___| | |
+    |  _ <| | '_ \\___ \   | |  |  __|   / /\ \ |  <    \___ \| '_ \ / _ \ | |
+    | |_) | | |_) |___) |  | |  | |____ / ____ \| . \   ____) | | | |  __/ | |
+    |____/|_|_.__/_____/   |_|  |______/_/    \_\_|\_\ |_____/|_| |_|\___|_|_|
+                                                                            
+    Welcome to BibStShell! Type 'help' to list commands.
+    The current/last working directory is {get_working_directory_path()}
+    If you want to change it use the set_directory <source_directory> command
+    and add the absolute path as an argument.
+    """   
     prompt = "BibStShell> "
     
-    # commands
-        
+    # commands  
     def do_load(self, arg):
         load_file_to_storage(arg)
         
     def do_storage(self, arg):
         display_storage_files()
         
+    def do_wd(self, arg):
+        print(f"Current working directory: {get_working_directory_path()}")
+        
+    def do_set_directory(self, arg):
+        set_directory(arg)
+        
     def do_help(self, arg):
         display_help_commands()
 
     def do_quit(self, arg):
-        print("Bye! - SHell closed")
+        print("Bye! - Shell closed")
         return True  # returning True exits the loop
 
 if __name__ == "__main__":

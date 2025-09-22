635393eda9329986cc305c22a525dc04389bfca5
Lisa tb <lisatebraak@gmail.com>
2025-09-22 15:43:32 +0200

    Sorting by alphabetical order (!!!) of reftype

--- GroupByRefType.py
+++ GroupByRefType.py
@@ -1,3 +1,24 @@
 
 def groupByRefType(refs):
     print(refs)
+    print("hellooo")
+    allrefs = {} #create dict to store {reftype: [ref1, ref2, ref3, etc]}
+    for (reftype, key), fields in refs.items():
+        print(allrefs.keys())
+        if reftype not in allrefs.keys():
+            #create new entry
+            newRefsArray = []
+            entry = ((reftype, key), fields)
+            newRefsArray.append(entry)
+            allrefs.update({reftype: newRefsArray})
+        else:
+            refsArray = allrefs.get(reftype)
+            refsArray.append(((reftype, key), fields))
+    allrefs = sorted(allrefs.items())
+
+    result = {}
+    for (key, refs) in allrefs:
+        for (key, value) in refs:
+            result.update({key:value})
+    print(result)
+    return result
--- test.py
+++ test.py
@@ -3,27 +3,31 @@ import difflib
 import batch_editor
 import file_generator
 import file_parser
+import GroupByRefType
 
 bib_examples_original = "testfiles/biblatex-examples.bib"
 bib_examples_generated = "testfiles/biblatex-examples-generated.bib"
 bib_examples_edited = "testfiles/biblatex-examples-edited.bib"
+bib_examples_grouped = "testfiles/biblatex-examples-grouped.bib"
 bib_tests = "testfiles/bibtests.bib"
 
-
+#Create 2 dictonaries, examples from "bib_examples_original" and test from "bib_tests"
 examples = file_parser.parse_bib(bib_examples_original, False)
 test = file_parser.parse_bib(bib_tests, True)
 
-pprint.pprint(examples)
-print(test)
+# pprint.pprint(examples)
+# print(test)
 
 # Some examples on how to access information from the dictionary.
 # print(result[("book", "augustine")]["author"])
 # print(result[("book", "cicero")]["annotation"])
 
 # Generate file from the dictionary:
-file_generator.generate_bib(bib_examples_generated, examples, 15)
+# file_generator.generate_bib(bib_examples_generated, examples, 15)
 
-# file_generator.generate_bib(bib_examples_grouped, examples, 15)
+groupedExamples = GroupByRefType.groupByRefType(examples)
+
+file_generator.generate_bib(bib_examples_grouped, groupedExamples, 15)
 
 # # Print the differences between the original file and the generated file:
 # with open(bib_examples_original) as original:

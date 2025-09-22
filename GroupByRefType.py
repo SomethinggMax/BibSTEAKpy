2a57d73eb64d45086d860ed81310c81830071294
Edwin Martens <edwinolofmartens@gmail.com>
2025-09-22 16:56:46 +0200

    Added ability to sort references by type

---
+++ GroupByRefType.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+class GroupingType(Enum):
+    ATOZ = 0
+    ZTOA = 1
+
+def groupByRefType(refs, GroupingType):
+
+    # create dict to store {reftype: [ref1, ref2, ref3, etc]}
+    allrefs = {}
+    for (reftype, key), fields in refs.items():
+        if reftype not in allrefs.keys():
+            #create new entry
+            newRefsArray = []
+            entry = ((reftype, key), fields)
+            newRefsArray.append(entry)
+            allrefs.update({reftype: newRefsArray})
+        else:
+            refsArray = allrefs.get(reftype)
+            refsArray.append(((reftype, key), fields))
+
+    #sort by tuple (alphabetically for now)
+    strings_only_arr = [('string', allrefs.get("string"))]
+    allrefs.pop("string")
+    allrefs = sorted(allrefs.items(), reverse=GroupingType.value)
+
+    #get the refs from the [ref1, ref2, ref3, etc] array and put them in a dict to return
+    result = {}
+
+    for (key, refs) in strings_only_arr + allrefs:
+        for (key, value) in refs:
+            result.update({key:value})
+
+    return result

f7f0999bfd7ab9714ccb66203b1905cb6bc8497e
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-09-30 20:15:51 +0200

    Fixed merge.py implementation

--- utils/merge.py
+++ utils/merge.py
@@ -6,7 +6,7 @@ def merge_reference(reference_1, reference_2) -> Reference:
     reference_1_fields = reference_1.get_fields()
     reference_2_fields = reference_2.get_fields()
 
-    for field_type, data in reference_1_fields().items():
+    for field_type, data in reference_1_fields.items():
         if field_type in reference_2_fields:
             if data != reference_2_fields[field_type]:
                 print("Duplicate fields do not have the same contents!")
@@ -27,7 +27,7 @@ def merge_files(bib_file_1, bib_file_2, file_name) -> BibFile:
     merged_bib_file.content.extend(bib_file_2.get_strings())
 
     # Add references from bib file 1.
-    for entry in bib_file_1:
+    for entry in bib_file_1.content:
         if type(entry) is Reference:
             if entry.cite_key in bib_file_2.get_cite_keys():
                 merged_reference = merge_reference(entry, bib_file_2.get_reference(entry.cite_key))
@@ -36,7 +36,7 @@ def merge_files(bib_file_1, bib_file_2, file_name) -> BibFile:
                 merged_bib_file.content.append(entry)
 
     # Add remaining references from bib file 2.
-    for entry in bib_file_2:
+    for entry in bib_file_2.content:
         if type(entry) is Reference:
             if entry.cite_key not in merged_bib_file.get_cite_keys():
                 merged_bib_file.content.append(entry)

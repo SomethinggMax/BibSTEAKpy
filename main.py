d3ceadb9eb94b019fdd5ca4c4e99e6f87178259d
Fabian <tabreacode@gmail.com>
2025-10-19 14:23:43 +0200

    changes to main

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from cli_main import start_cli
 import subprocess
 
+# Added Something
+
 def main():
     print("Choose the type of interface you would like to use now")
     print("g --- Graphical User Interface")
+    
     print("c --- Command Line Interface")
 
     while True:
         choice = input("\nEnter your choice (g/c): ").strip().lower()
 
         if choice == "g":
-            print("Starting GUI")
+            
+            print("SARTING.....")
             subprocess.run(["python", "gui_main.py"])
             break
         elif choice == "c":
             print("Starting CLI")
             start_cli()
+            print("STAAART")
             break
         else:
+            print("yoo")
             print("\n Invalid input, please enter 'g' for GUI or 'c' for CLI\n")
 
 if __name__ == "__main__":

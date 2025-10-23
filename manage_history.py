6a4595e0c386856189cf3061910a411b8b93c35f
Fabian <tabreacode@gmail.com>
2025-10-23 22:03:15 +0200

    Commits are done now only if there are real changes done to the file.

--- manage_history.py
+++ manage_history.py
@@ -38,42 +38,53 @@ def commit(bibfile: BibFile):
     
     
     # do commit
+
+    
     with open(tracker_file_path, "r", encoding="utf-8") as tracker_file:
         tracker = json.load(tracker_file)
         tracker_file.close()
         
-    with open(tracker_file_path, "w", encoding="utf-8") as tracker_file:
-        counter = tracker["counter"]
+    has_file = any(os.path.isfile(os.path.join(hist_dir_path, name)) and name != "tracker.json" for name in os.listdir(hist_dir_path))
+    
+    if has_file:
+        last_commit_path = os.path.join(hist_dir_path, f"c_{tracker['head']}_{file_name}")
+        last_commit_bib = file_parser.parse_bib(last_commit_path, True)
+    else:
+        last_commit_bib = bibfile # To avoid crashing
         
-        if tracker["branch_tip"] == tracker["head"]:
-            tracker["counter"] = counter + 1
-            tracker["branch_tip"] = counter + 1
-            tracker["head"] = counter + 1
+    if not has_file or not same_commit(bibfile, last_commit_bib):
+        with open(tracker_file_path, "w", encoding="utf-8") as tracker_file:
+            counter = tracker["counter"]
             
-            commit_name = f"c_{tracker['head']}_{file_name}"
-            
-            commit_file_path = os.path.join(hist_dir_path, commit_name)
-            file_generator.generate_bib(bibfile, commit_file_path, True)
-            
-            json_str= json.dumps(tracker, indent=2)
-            
-        else:
-            delete_branch(hist_dir_path, file_name, tracker["head"] + 1, tracker["branch_tip"])
-            
-            tracker["counter"] = tracker["head"] + 1
-            tracker["branch_tip"] = tracker["head"] + 1
-            tracker["head"] = tracker["head"] + 1
-            
-            
-            commit_name = f"c_{tracker['head']}_{file_name}"
-            commit_file_path = os.path.join(hist_dir_path, commit_name)
-            file_generator.generate_bib(bibfile, commit_file_path, True)
-            json_str= json.dumps(tracker, indent=2)
-            
-            
-        
-        tracker_file.write(json_str)
-        tracker_file.close()
+            if tracker["branch_tip"] == tracker["head"]:
+                tracker["counter"] = counter + 1
+                tracker["branch_tip"] = counter + 1
+                tracker["head"] = counter + 1
+                
+                commit_name = f"c_{tracker['head']}_{file_name}"
+                
+                commit_file_path = os.path.join(hist_dir_path, commit_name)
+                file_generator.generate_bib(bibfile, commit_file_path, True)
+                
+                json_str= json.dumps(tracker, indent=2)
+                
+            else:
+                delete_branch(hist_dir_path, file_name, tracker["head"] + 1, tracker["branch_tip"])
+                
+                tracker["counter"] = tracker["head"] + 1
+                tracker["branch_tip"] = tracker["head"] + 1
+                tracker["head"] = tracker["head"] + 1
+                
+                
+                commit_name = f"c_{tracker['head']}_{file_name}"
+                commit_file_path = os.path.join(hist_dir_path, commit_name)
+                file_generator.generate_bib(bibfile, commit_file_path, True)
+                json_str= json.dumps(tracker, indent=2)
+                
+            tracker_file.write(json_str)
+            tracker_file.close()
+    else:
+        print_in_yellow("No changes detected.")
         
         
 def undo(bibfile: BibFile, step=1):
@@ -158,7 +169,74 @@ def redo(bibfile: BibFile, step=1):
         return
         
         
+def same_commit(bib_file1: BibFile, bib_file2: BibFile):
+    same_r = same_references(bib_file1, bib_file2)
+    same_c = same_comments(bib_file1, bib_file2)
+    same_p = same_preambles(bib_file1, bib_file2)
+    same_s = same_strings(bib_file1, bib_file2)
+    
+    return same_r and same_c and same_p and same_s
 
+    
+def same_references(bib_file1: BibFile, bib_file2:BibFile):
+    entries1 = bib_file1.get_references()
+    entries2 = bib_file2.get_references()
+
+    string_entries1 = []
+    for entry in entries1:
+        fields = entry.get_fields()
+        field_strings = [f"{key}: {value}" for key, value in fields.items() if
+                         key not in ["comment_above_reference", "entry_type"]]
+        string_entries1.append(field_strings)
+        
+        
+    string_entries2 = []
+    for entry in entries2:
+        fields = entry.get_fields()
+        field_strings = [f"{key}: {value}" for key, value in fields.items() if
+                         key not in ["comment_above_reference", "entry_type"]]
+        string_entries2.append(field_strings)
+        
+    return string_entries2 == string_entries1
+
+
+def same_comments(bib_file1: BibFile, bib_file2:BibFile):
+    comments1 = bib_file1.get_comments()
+    comments2 = bib_file2.get_comments()
+    
+    string_comments1 = [com.comment for com in comments1]
+    string_comments2 = [com.comment for com in comments2]
+    
+    return string_comments1 == string_comments2
+
+
+def same_preambles(bib_file1: BibFile, bib_file2:BibFile):
+    comments1 = bib_file1.get_preambles()
+    comments2 = bib_file2.get_preambles()
+    
+    string_comments1 = [com.preamble for com in comments1]
+    string_comments2 = [com.preamble for com in comments2]
+    
+    return string_comments1 == string_comments2
+    
+    
+def same_strings(bib_file1: BibFile, bib_file2:BibFile):
+    entries1 = bib_file1.get_strings()
+    entries2 = bib_file2.get_strings()
+    
+    string_entries1 = []
+    for entry in entries1:
+        fields = entry.get_fields()
+        field_strings = [f"{key}: {value}" for key, value in fields.items()]
+        string_entries1.append(field_strings)
+
+    string_entries2 = []
+    for entry in entries2:
+        fields = entry.get_fields()
+        field_strings = [f"{key}: {value}" for key, value in fields.items()]
+        string_entries2.append(field_strings)
+        
+    return string_entries2 == string_entries1
         
         
         

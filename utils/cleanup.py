7e208e627cb631ffa4563011b870bac08f016dd2
Fabian <tabreacode@gmail.com>
2025-10-28 17:11:38 +0100

    Merge remote-tracking branch 'origin/main' into HEAD

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -3,6 +3,48 @@ from objects import BibFile, Reference, String, Comment
 from utils import json_loader
 
 
+def convert_symbols(reference: Reference):
+    special_latex_to_unicode = {
+        '{\\ss}': 'ß', '\\ss': 'ß',
+        '{\\o}': 'ø', '\\o': 'ø',
+        '{\\O}': 'Ø', '\\O': 'Ø',
+        '{\\ae}': 'æ', '\\ae': 'æ',
+        '{\\AE}': 'Æ', '\\AE': 'Æ',
+        '{\\l}': 'ł', '\\l': 'ł',
+        '{\\L}': 'Ł', '\\L': 'Ł'
+    }
+    latex_to_unicode = {
+        "\\`": ('\u0300', ['a', 'e', 'i', 'o', 'u']),  # Grave accent
+        "\\'": ('\u0301', ['a', 'e', 'i', 'o', 'u', 'y', 'c']),  # Acute accent
+        "\\^": ('\u0302', ['a', 'e', 'i', 'o', 'u']),  # Circumflex accent
+        "\\~": ('\u0303', ['a', 'n', 'o']),  # Tilde
+        "\\=": ('\u0304', ['a', 'e', 'i', 'o', 'u', 'p']),  # Macron
+        "\\u": ('\u0306', ['a', 'e', 'i', 'o', 'u']),  # Breve
+        "\\.": ('\u0307', ['o']),  # Dot above
+        '\\"': ('\u0308', ['a', 'e', 'i', 'o', 'u']),  # Diaeresis
+        "\\a": ('\u030A', ['a']), "\\r": ('\u030A', ['a']),  # Ring above
+        "\\H": ('\u030B', ['o', 'u']),  # Double acute accent
+        "\\v": ('\u030C', ['c', 'r', 's', 'z']),  # Caron
+        "\\c": ('\u0327', ['c', 's']),  # Cedilla
+        "\\k": ('\u030C', ['a', 'e', 'i', 'o', 'u']),  # Ogonek
+    }
+    for field_type, data in reference.get_fields().items():
+        if field_type == "comment_above_reference" or field_type == "entry_type" or field_type == "cite_key":
+            continue
+        for key, value in special_latex_to_unicode.items():
+            data = data.replace(key, value)
+        for latex_command, (combining_mark, letters) in latex_to_unicode.items():
+            capitalized = [x.capitalize() for x in letters]
+            for letter in letters + capitalized:
+                data = data.replace('{' + latex_command + '{\\' + letter + '}}', letter + combining_mark)
+                data = data.replace(latex_command + '{\\' + letter + '}', letter + combining_mark)
+                data = data.replace('{' + latex_command + '{' + letter + '}}', letter + combining_mark)
+                data = data.replace(latex_command + '{' + letter + '}', letter + combining_mark)
+                data = data.replace('{' + latex_command + '\\' + letter + '}', letter + combining_mark)
+                data = data.replace('{' + latex_command + letter + '}', letter + combining_mark)
+                data = data.replace('{' + latex_command + ' ' + letter + '}', letter + combining_mark)
+
+
 def clean_url_and_doi(reference: Reference, use_url, use_doi) -> Reference:
     fields = reference.get_fields()
     url_field = fields.get("url")
@@ -56,16 +98,79 @@ def lower_fields(reference: Reference):
         setattr(reference, field_type.lower(), data)
 
 
+def change_field_enclosure(reference: Reference, start_enclosure: str, end_enclosure: str):
+    for field_type, data in reference.get_fields().items():
+        if field_type == "comment_above_reference" or field_type == "entry_type" or field_type == "cite_key":
+            continue
+        if ' # ' in data:
+            continue
+        new_data = remove_enclosure(data)
+        if new_data == data:
+            continue
+        if not new_data.isdigit():
+            new_data = start_enclosure + new_data + end_enclosure
+        setattr(reference, field_type, new_data)
+
+
+def remove_enclosure(field_value: str) -> str:
+    start_enclosure = ""
+    end_enclosure = ""
+    for char in field_value:
+        if char == '{':
+            start_enclosure += char
+        elif char == '"':
+            start_enclosure += char
+        else:
+            break
+    for char in reversed(field_value):
+        if char == '}':
+            end_enclosure += '{'
+        elif char == '"':
+            end_enclosure += char
+        else:
+            break
+
+    # Remove enclosure while start and end enclosure are equal.
+    result = field_value
+    for (start, end) in zip(start_enclosure, end_enclosure):
+        if start == end:
+            result = result[1:-1]
+        else:
+            break
+
+    # Check if braces are still correct.
+    for x in range(5):
+        braces_level = 0
+        for char in result:
+            if char == '{':
+                braces_level += 1
+            elif char == '}':
+                braces_level -= 1
+            if braces_level < 0:
+                break
+
+        if braces_level != 0:
+            if x > 3:
+                return field_value  # Give up on changing anything and just return the original value.
+            result = "{" + result + "}"  # Add a pair of braces and hope it fixes the problem.
+        else:
+            break
+    return result
+
+
 def cleanup(bib_file: BibFile):
     config = json_loader.load_config()
 
     # Load values from config (or default values).
+    convert_special_symbols = config.get("convert_special_symbols", False)
     url = config.get("use_url", True)
     doi = config.get("use_doi", True)
     comments = config.get("comments", True)
     comment_entries = config.get("comment_entries", True)
     lowercase_entry_types = config.get("lowercase_entry_types", False)
     lowercase_fields = config.get("lowercase_fields", False)
+    braces_enclosure = config.get("braces_enclosure", False)
+    quotation_marks_enclosure = config.get("quotation_marks_enclosure", False)
 
     fields = config.get("unnecessary_fields", [])
 
@@ -82,5 +187,13 @@ def cleanup(bib_file: BibFile):
                 lower_entry_type(entry)
             if lowercase_fields:
                 lower_fields(entry)
+            if braces_enclosure and quotation_marks_enclosure:
+                raise ValueError("Config file has invalid enclosures set, set only one to true.")
+            if convert_special_symbols:
+                convert_symbols(entry)
+            if braces_enclosure:
+                change_field_enclosure(entry, '{', '}')
+            if quotation_marks_enclosure:
+                change_field_enclosure(entry, '"', '"')
 
     return bib_file

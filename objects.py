7fa453dbb195d6b45d935b2df32832a0d7d1ef5b
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-10-30 17:04:37 +0100

    Added equality to BibFile object. Two objects with the same content are now the same.

--- objects.py
+++ objects.py
@@ -6,6 +6,11 @@ class BibFile(object):
         self.file_name = file_name
         self.content = list()
 
+    def __eq__(self, other):
+        if not isinstance(other, BibFile):
+            return NotImplemented
+        return self.file_name == other.file_name and self.content == other.content
+
     def get_cite_keys(self):
         cite_keys = []
         for entry in self.content:
@@ -51,10 +56,20 @@ class Comment(object):
     def __init__(self, comment):
         self.comment = comment
 
+    def __eq__(self, other):
+        if not isinstance(other, Comment):
+            return NotImplemented
+        return self.comment == other.comment
+
 
 class Preamble(object):
     def __init__(self, preamble):
         self.preamble = preamble
+
+    def __eq__(self, other):
+        if not isinstance(other, Preamble):
+            return NotImplemented
+        return self.preamble == other.preamble
 
 
 class Enclosure(Enum):
@@ -68,7 +83,13 @@ class String(object):
         self.abbreviation = abbreviation
         self.long_form = long_form
         self.enclosure = enclosure
-        
+
+    def __eq__(self, other):
+        if not isinstance(other, String):
+            return NotImplemented
+        return (self.comment_above_string == other.comment_above_string and self.abbreviation == other.abbreviation
+                and self.long_form == other.long_form and self.enclosure == other.enclosure)
+
     def get_fields(self):
         return vars(self)
 
@@ -78,6 +99,15 @@ class Reference(object):
         self.comment_above_reference = comment_above_reference
         self.entry_type = entry_type
         self.cite_key = cite_key
+
+    def __eq__(self, other):
+        if not isinstance(other, Reference):
+            return NotImplemented
+        for (self_field, self_value), (other_field, other_value) in (
+                zip(self.get_fields().items(), other.get_fields().items())):
+            if self_field != other_field or self_value != other_value:
+                return False
+        return True
 
     def get_fields(self):
         return vars(self)
@@ -91,7 +121,7 @@ class Reference(object):
 
     def __repr__(self):
         return self.__str__()
-    
+
 
 class GraphNode(object):
     def __init__(self, title):
@@ -128,3 +158,23 @@ if __name__ == "__main__":
 
     random_file.content.append(example_reference)
     print(random_file.content)
+
+    second_reference = Reference("", "article", "au32")
+    second_reference.author = "George"
+    second_reference.year = 2004
+    second_reference.custom_attribute = "custom value"
+    assert example_reference == second_reference
+    second_reference.author = "Not George"
+    assert example_reference != second_reference
+    delattr(second_reference, "author")
+    setattr(second_reference, "author", "George")
+    assert example_reference != second_reference  # Still false since the order is different now.
+    delattr(example_reference, "author")
+    delattr(second_reference, "author")
+    assert example_reference == second_reference  # True since author field is gone
+    setattr(example_reference, "Author", "George")
+    setattr(second_reference, "author", "George")
+    assert example_reference != second_reference  # Fields are different
+    delattr(example_reference, "Author")
+    setattr(example_reference, "author", "George")
+    assert example_reference == second_reference # Now they are the same again

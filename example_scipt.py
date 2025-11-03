da922309d3897cf446535a961a5eda1dab4d2970
Wouter van Gent <e.w.vangent@student.utwente.nl>
2025-11-03 12:52:50 +0100

    Added script for clean, enr, clean and col.

---
+++ example_scipt.py
@@ -0,0 +1,26 @@
+import argparse
+import history_manager
+from utils import file_parser, file_generator, enrichment, cleanup, abbreviations_exec
+
+
+def enr_clean_col(absolute_path):
+    bib_file = file_parser.parse_bib(absolute_path)
+    history_manager.initialise_history(bib_file)
+
+    cleanup.cleanup(bib_file)
+    enrichment.sanitize_bib_file(bib_file)
+    cleanup.cleanup(bib_file)
+    abbreviations_exec.execute_abbreviations(bib_file, True, 10000)
+
+    file_generator.generate_bib(bib_file, absolute_path)
+    history_manager.commit(bib_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Script that enriches, cleans and collapses original file, while saving history."
+    )
+    parser.add_argument("-path", required=True, type=str)
+    args = parser.parse_args()
+
+    enr_clean_col(args.path)
